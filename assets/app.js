2f5b9b5f74e2267a8309ce4a10ed29290cceae19
ElyasNil <r.elyas.nil@gmail.com>
2024-12-03 10:11:07 +0300

    revenir surimportmap

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,12 @@ if (typeof window.jQuery === 'undefined') {
     window.$ = window.jQuery = $;
 } // Assurez-vous que jQuery est globalement accessible
 import './bootstrap.js';
-import './bootstrap-5-dist/js/bootstrap.bundle.js';
+import './bootstrap/dist/js/bootstrap.bundle.js';
 import './DataTables/datatables.js';
 import './DataTables/datatables.min.js';
 
 import './styles/app.css';
-import './bootstrap-5-dist/css/bootstrap.min.css';
+import './bootstrap/dist/css/bootstrap.min.css';
 import './DataTables/datatables.css';
 
 import 'datatables.net';
